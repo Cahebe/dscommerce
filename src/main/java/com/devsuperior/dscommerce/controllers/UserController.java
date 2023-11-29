1399f18cbe66989437fdbf92675e2e0658450c05
Cahebe <cahebe@gmail.com>
2023-11-28 21:31:26 -0300

    Método para salvar novo pedido criado.

--- src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService service;
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
     @GetMapping(value = "/me")
     public ResponseEntity<UserDTO> getMe() {
         UserDTO dto = service.getMe();
